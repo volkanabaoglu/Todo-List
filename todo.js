570b3247136346d15182592c61bd6a3b8116ce11
Volkan <83585653+volkanabaoglu@users.noreply.github.com>
2023-04-29 22:48:17 +0300

    29/04

---
+++ todo.js
@@ -0,0 +1,96 @@
+const form = document.querySelector("#todo-form");
+const todoInput = document.querySelector("#todo");
+const todoList = document.querySelector(".list-group");
+const firstCardBody = document.querySelectorAll(".card-body")[0];
+const secondCardBody = document.querySelectorAll(".card-body")[1];
+const filter = document.querySelector("#filter");
+const clearButton = document.querySelector("#clear-todos")
+
+eventListener();
+
+function eventListener (){
+    form.addEventListener("submit",addTodo);
+    document.addEventListener("DOMContentLoaded", (event) => {
+        addTodosOnRefresh();
+      });
+}
+
+function addTodosOnRefresh(){
+    let todos = getTodosFromStorage();
+    
+    if (todos === null || todos.length === 0) {
+        // Depolama alanında hiç görev kaydedilmemiş
+        alert('Depolama alanında görev bulunmuyor!');
+        return;
+    }
+
+    else{
+        todos.forEach(function(todo){
+            addTodoToUI(todo);
+        });
+    }
+}
+
+
+function addTodo (e){
+    e.preventDefault();
+    const newTodo = todoInput.value.trim();//trim => baş ve sondaki boşlukları siler.
+    if(newTodo===""){
+        showAlert("danger","Lütfen bir to-do giriniz!");
+    }
+    else{
+        addTodoToUI(newTodo);
+        addTodoToStorage(newTodo);
+        showAlert("success","Başarılı..");
+    }
+
+}
+
+function showAlert(type,message){
+        const alertItem = document.createElement("div");
+        alertItem.className = `alert alert-${type}`;
+        alertItem.textContent = message;
+        firstCardBody.appendChild(alertItem);
+
+        setTimeout(() => {
+            alertItem.remove();
+        }, 1500);
+
+}
+
+
+function addTodoToUI(newTodo){
+    const listItem = document.createElement("li");
+    listItem.className = "list-group-item d-flex justify-content-between";
+    
+    const link = document.createElement("a");
+    link.href = "#";
+    link.className = "delete-item";
+    link.innerHTML = '<i class = "fa fa-remove"></i>' ;
+
+    listItem.appendChild(document.createTextNode(newTodo));
+    listItem.appendChild(link);
+
+    todoList.appendChild(listItem);
+    todoInput.value = "";
+}
+
+function getTodosFromStorage(){
+    let todos;
+
+    if(localStorage.getItem("todos")===null){
+        todos = [];
+    }
+    else{
+        todos= JSON.parse(localStorage.getItem("todos"));
+    }
+    return todos;
+}
+
+
+function addTodoToStorage(newTodo){
+    let todos = getTodosFromStorage();
+    todos.push(newTodo);
+    localStorage.setItem("todos",JSON.stringify(todos));
+}
+
